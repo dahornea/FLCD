7e0212916c22851dddcee82b3b42dba436ef5060
913-Hornea-Dorian-Alexandru <alex.1375@yahoo.com>
2023-10-29 19:09:23 +0200

    Create Pair.cs

---
+++ Lab2/Lab2/Pair.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2bun
+{
+    public class Pair<First, Second>
+    {
+        public First _first { get; }
+        public Second _second { get; }
+
+        public Pair(First first, Second second)
+        {
+            _first = first;
+            _second = second;
+        }
+
+        public override string ToString()
+        {
+            return $"Pair{{ first = {_first}, second = {_second} }}";
+        }
+    }
+
+}
